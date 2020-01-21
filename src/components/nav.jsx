520d8d643c18efbaa41acd219ac53c4bad638ba9
cskhw <51100329+cskhw@users.noreply.github.com>
2020-01-21 09:31:53 +0900

    change nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from '../container/login';
-import Auth from '../container/auth';
 import Main from './main';
 import logo from '../public/rootlogo.png'
 export default class Nav extends Component {
@@ -21,7 +20,6 @@ export default class Nav extends Component {
                     <a href="load">오시는길</a>
                     <a href="login">로그인</a>
                 </nav>
-                <Route path="/auth" component={Auth}></Route>
                 <Route path="/login" component={Login}></Route>
                 <Route path="/" component={Main}></Route>
             </Router>
