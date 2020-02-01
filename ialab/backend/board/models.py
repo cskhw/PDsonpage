15cb26c67389ba53374f695590ddbb13a6232cc9
cskhw <gusdn0828@gmail.com>
2020-02-02 03:08:41 +0900

    #22 change server

---
+++ ialab/backend/board/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Board(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=40)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    author = models.CharField(max_length=16)
+    admin = models.CharField(max_length=16)
+
+    def __str(self):
+        return self.title
---
+++ ialab/backend/board/serializers.py
@@ -0,0 +1,9 @@
+from board.models import Board
+from rest_framework import serializers
+
+
+class BoardSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Board
+        fields = ('id', 'title', 'description',
+                 'date', 'update', 'author', 'admin')
